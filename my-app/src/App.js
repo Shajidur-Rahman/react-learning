43cd28f9fbb0b28bb3362a20911e4566a0089408
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-12 17:35:43 +0600

    using huks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,42 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Header from "./MyComponents/header";
-import Todo from "./MyComponents/Todos";
-import TodoItems from "./MyComponents/TodoItmes";
+import { Todos } from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
+import React, { useState } from 'react';
+
 
 
 
 function App() {
+  const ondelet = (todo)=>{
+    console.log('i am ondelet of ', todo);
+    let index = todos.indexOf(todo)
+    todo.splice(index, 1)
+  }
 
-  let todo = [
+  let todos = [
     {
-      sno: 1,
-      title: 'go to the market',
-      des: 'you need to bye some thing'
+      sn: 1,
+      title:'go to the market',
+      des: 'you need to go to the market'
     },
     {
-      sno: 2,
-      title: 'go to the market',
-      des: 'you need to bye some thing'
+      sn: 2,
+      title:'go to the collage',
+      des: 'you need to go to the collage'
     },
     {
-      sno: 3,
-      title: 'go to the market',
-      des: 'you need to bye some thing'
+      sn: 3,
+      title:'go to the school',
+      des: 'you need to go to the school'
     }
   ]
+
   return (
     <>
     <Header />
-    <Todo todo = {todo}/>
+    <Todos todos={todos} ondelet={ondelet} />
     <Footer/>
     </>
   );
